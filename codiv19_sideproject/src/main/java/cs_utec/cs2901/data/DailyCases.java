6a85a5238dbf531cdf81de9f88c84388f9a40101
Angel Motta <angelmotta@gmail.com>
2020-04-03 07:57:26 -0500

    DailyCases

--- codiv19_sideproject/src/main/java/cs_utec/cs2901/data/DailyCases.java
+++ codiv19_sideproject/src/main/java/cs_utec/cs2901/data/DailyCases.java
@@ -3,10 +3,12 @@ package cs_utec.cs2901.data;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Date;
 
 @Entity
+@Table(name = "dailycases")
 public class DailyCases implements Serializable {
 
     @Id
